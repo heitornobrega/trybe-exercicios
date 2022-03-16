c4756a407cc79b094f59650b26393f5c5bdf8b04
Heitor Nóbrega Tico <nobheitor@gmail.com>
2022-03-16 10:30:59 -0300

    eventos onload e onclick

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -0,0 +1,14 @@
+window.onload = jsCarregado
+
+function jsCarregado(){
+    alert("Página carregada.")
+}
+
+console.log("oi");
+
+function maisInfos(){
+    let textoMaisInfo = document.getElementById("trybe-continua");
+    textoMaisInfo.innerText = "Hello world";
+    console.log("Hello world");
+
+}
