f426718261c53690f4c3aba7a5681e1995ae38f8
Heitor Nóbrega Tico <nobheitor@gmail.com>
2022-03-17 05:55:48 -0300

    alteracao

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -52,11 +52,26 @@ function createDaysOfTheWeek() {
 
 function feriados(Feriados){
   const btnFeriado = document.createElement('button');
-  buttonsContainer = document.getElementsByClassName("buttons-container")[0];
+  const buttonsContainer = document.getElementsByClassName("buttons-container")[0];
   btnFeriado.id = "btn-holiday";
   btnFeriado.innerText = Feriados;
   buttonsContainer.appendChild(btnFeriado);
+};
 
-
-}
 feriados("Feriados");
+
+// Exercício 3:
+// Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday" .
+// É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)" .
+
+function mudaCorBtn(idBtn){
+  const btn = document.querySelector(`#${idBtn}`);
+  btn.addEventListener('click', function(){
+      for(let c = 0; c <= 3; c++){
+      const holidays = document.querySelectorAll(".holiday")[c];
+      holidays.style.backgroundColor = 'green';}
+
+  })
+}
+
+mudaCorBtn("btn-holiday");
