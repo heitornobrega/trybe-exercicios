5b3fdbe4280d0675a282817b677bfd3ae6a199c4
heitornobrega <nobheitor@gmail.com>
2022-04-06 06:24:18 -0300

    exercicio 1.3

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -7,4 +7,11 @@ describe('Teste as somas', () => {
     it('A soma entre 0 e 0 deve ser 0', () => {
         expect(sum(0, 0)).toBe(0);
     })
+    it('A função sum lança um erro quando os parâmetros são 4 e "5"', () => {
+        expect(() => { sum(4, '5') }).toThrow();
+
+    })
+    it('A mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
+        expect(() => { sum(4, '5') }).toThrow(new Error('parameters must be numbers'));
+    })
 })
