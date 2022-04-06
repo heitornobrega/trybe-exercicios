95155419a3b0934af8cf23a514c17bcfcc6a155c
heitornobrega <nobheitor@gmail.com>
2022-04-06 08:01:02 -0300

    exercicio 3.4

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/myFizzBuzz.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/myFizzBuzz.test.js
@@ -12,4 +12,9 @@ describe('testa a função myFizzBuzz', () => {
   it('Faça uma chamada com um número divisível por 5 e verifique se o retorno é o esperado', () => {
     expect(myFizzBuzz(10)).toBe('buzz');
   })
+  
+  it('Faça uma chamada com um número que não é divisível por 3 ou 5 e verifique se o retorno é o esperado', () => {
+    expect(myFizzBuzz(4)).toBe(4);
+  })
+
 })
