b1f7cb101f6fa5656cd74fac8cb6536c963cee32
heitornobrega <nobheitor@gmail.com>
2022-04-06 06:57:45 -0300

    exercicio 2.2

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/myRemove.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/myRemove.test.js
@@ -4,4 +4,8 @@ describe('A função myRemove(arr, item) recebe um array arr e retorna uma cópi
     it('A chamada myRemove([1, 2, 3, 4], 3) deve retornar [1, 2, 4]', () => {
         expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
     })
+    it('A chamada myRemove([1, 2, 3, 4], 3) não deve retornar [1, 2, 3, 4]', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    })
+
 })
