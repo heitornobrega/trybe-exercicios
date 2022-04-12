303a117b47815599cdaab8f82c2de38696bd5973
heitornobrega <nobheitor@gmail.com>
2022-04-12 09:58:51 -0300

    exemplo 3 com segundo parametro alterado

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exemploReduce3.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exemploReduce3.js
@@ -2,5 +2,6 @@ const numbers = [50, 85, -30, 3, 15];
 
 const getBigger = (acc, element) => ((acc > element) ? acc : element);
 
+console.log(numbers.reduce(getBigger));
 
-console.log(numbers.reduce(getBigger))
+console.log(numbers.reduce(getBigger, 100));
