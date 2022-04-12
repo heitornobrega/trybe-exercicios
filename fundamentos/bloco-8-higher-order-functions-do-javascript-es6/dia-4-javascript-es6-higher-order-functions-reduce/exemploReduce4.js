d7c47c2eb0d8f8428b9541aa076a3e56c21e5475
heitornobrega <nobheitor@gmail.com>
2022-04-12 10:44:03 -0300

    exemplo 4 segunda parte

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exemploReduce4.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exemploReduce4.js
@@ -6,3 +6,11 @@ const somaNums = (acc, element) => acc + element;
 const somaPares = (array) => array.filter(pegaPares).reduce(somaNums);
 
 console.log(somaPares(numbers));
+
+const sum2 = (acc, element) => (
+    (element % 2 === 0) ? acc + element : acc // se o elemento atual for par, some, caso contrario mantenha acc como é;
+);
+
+const newSum = (array) => array.reduce(sum2);
+
+console.log(newSum(numbers));
