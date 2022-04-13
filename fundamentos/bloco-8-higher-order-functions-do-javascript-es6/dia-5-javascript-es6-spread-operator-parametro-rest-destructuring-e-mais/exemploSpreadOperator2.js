b681063517025121c7bc1b911994762b5f5b8eeb
heitornobrega <nobheitor@gmail.com>
2022-04-13 10:22:18 -0300

    exemplo spread operator 2

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exemploSpreadOperator2.js
@@ -0,0 +1,7 @@
+const primavera = ['OUT', 'NOV', 'DEZ'];
+const verao = ['JAN', 'FEV', 'MAR'];
+const outono = ['ABR', 'MAI', 'JUN'];
+const inverno = ['JUL', 'AGO', 'SET'];
+
+const meses = [...verao, ...outono, ...inverno, ...primavera];
+console.log(meses);
