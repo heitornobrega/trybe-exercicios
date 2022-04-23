c2a96d078c7bf9ad344d483c4fc22bd64b1e6dde
heitornobrega <nobheitor@gmail.com>
2022-04-23 11:25:06 -0300

    exer 1

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/apiScript.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-asyncawait/apiScript.js
@@ -1,8 +1,16 @@
 // apiScript.js
 const API_URL = 'https://icanhazdadjoke.com/';
 
-const fetchJoke = () => {
-  // Adicionar lógica aqui!
-};
 
-window.onload = () => fetchJoke();
+const fetchJoke = () => {
+    const reqObj = {
+        method: 'GET',
+        headers: { 'Accept': 'application/json' }
+    };
+    fetch(API_URL, reqObj)
+        .then(value => value.json())
+        .then(dados => dados.joke)
+        .then(valor => document.getElementById('jokeContainer').innerText = valor)
+    
+};
+window.onload = () =>  fetchJoke();
