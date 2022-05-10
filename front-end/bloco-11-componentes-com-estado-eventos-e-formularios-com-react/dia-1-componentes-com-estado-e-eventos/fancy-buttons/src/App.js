1996591b4a79949eac9bb0dff9959ff4e3664a62
heitornobrega <nobheitor@gmail.com>
2022-05-10 18:39:26 -0300

    Estados de cor implementados

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 
 class Btn extends React.Component{
   render() {
-    const { nomeBtn, evento} = this.props;
+    const { nomeBtn, evento, classe} = this.props;
     return (
-      <button onClick={evento}>
+      <button onClick={evento} className={classe}>
         {nomeBtn}
     </button>
       );
@@ -19,35 +19,89 @@ class App extends React.Component{
     this.printTextA = this.printTextA.bind(this);
     this.printTextB = this.printTextB.bind(this);
     this.printTextC = this.printTextC.bind(this);
+    this.corBtnA = this.corBtnA.bind(this);
+    this.corBtnB = this.corBtnB.bind(this);
+    this.corBtnC = this.corBtnC.bind(this);
     this.state ={
       numCliquesA: 0,
       numCliquesB: 0,
-      numCliquesC: 0
+      numCliquesC: 0,
+      corAtualA: 'blue',
+      corAtualB: 'blue',
+      corAtualC: 'blue',
     }
   }
   printTextA() {
     this.setState((estadoAnterior, _props) =>
     ({numCliquesA: estadoAnterior.numCliquesA + 1})
     )
+    this.corBtnA();
   }
   printTextB() {
     this.setState((estadoAnteriorB, _props) =>
     ({numCliquesB: estadoAnteriorB.numCliquesB + 1})
     )
+    this.corBtnB();
   }
   
   printTextC() {
     this.setState((estadoAnteriorC, _props) =>
     ({numCliquesC: estadoAnteriorC.numCliquesC + 1})
     )
+    this.corBtnC();
+  }
+  corBtnA() {
+    this.setState((estadoCor, _props) => {
+      if (this.state.numCliquesA % 2 !== 0) {
+        return {corAtualA: estadoCor.corAtualA = 'red'}
+      } else {
+        return {corAtualA: estadoCor.corAtualA = 'blue'}
+      }
+    }
+    )
+  }
+
+  corBtnB() {
+    this.setState((estadoCor, _props) => {
+      if (this.state.numCliquesA % 2 !== 0) {
+        return {corAtualB: estadoCor.corAtualB = 'red'}
+      } else {
+        return {corAtualB: estadoCor.corAtualB = 'blue'}
+      }
+    }
+    )
+  }
+
+  corBtnC() {
+    this.setState((estadoCor, _props) => {
+      if (this.state.numCliquesC % 2 !== 0) {
+        return {corAtualC: estadoCor.corAtualC = 'red'}
+      } else {
+        return {corAtualC: estadoCor.corAtualC = 'blue'}
+      }
+    }
+    )
   }
   render() {
     // console.log(this.state.numCliques);
     return (
       <>
-      <Btn nomeBtn={this.state.numCliquesA} evento={this.printTextA}/>
-      <Btn nomeBtn={this.state.numCliquesB} evento={this.printTextB}/>
-      <Btn nomeBtn={this.state.numCliquesC} evento={this.printTextC}/>
+        <Btn
+          nomeBtn={this.state.numCliquesA}
+          evento={this.printTextA}
+          classe={this.state.corAtualA}
+        />
+        <Btn
+          nomeBtn={this.state.numCliquesB}
+          evento={this.printTextB}
+          classe={this.state.corAtualB}
+
+        />
+        <Btn
+          nomeBtn={this.state.numCliquesC}
+          evento={this.printTextC}
+          classe={this.state.corAtualC}
+        />
       </>
       );
   }
