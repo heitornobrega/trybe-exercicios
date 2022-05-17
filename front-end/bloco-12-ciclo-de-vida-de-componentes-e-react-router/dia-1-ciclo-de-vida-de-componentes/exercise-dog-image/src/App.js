2b0114f19a6730415819af62f823b384c5285a97
heitornobrega <nobheitor@gmail.com>
2022-05-17 15:14:14 -0300

    atualiza

--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercise-dog-image/src/App.js
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercise-dog-image/src/App.js
@@ -1,55 +1,50 @@
 import React from 'react';
-import './App.css';
-
 class App extends React.Component {
-  constructor() {
-    super()
-
-    this.fetchDogs = this.fetchDogs.bind(this);
-    this.renderDogImg = this.renderDogImg.bind(this);
-    this.chamaDog = this.chamaDog.bind(this);
-    
-    this.state = {
-      message: '',
-      status: undefined,
-    }
-
-  }
-  async fetchDogs() {
-    const requestReturn = await fetch('https://dog.ceo/api/breeds/image/random');
-    const requestobject = await requestReturn.json();
-    this.setState({
-      message: requestobject.message,
-      status: requestobject.status,
-    })
-  }
-
-  chamaDog() {
-    console.log('hhe dog')
-  }
-  componentDidMount(){
-    this.fetchDogs()
-  }
-
-  renderDogImg() {
-    const {message} = this.state
-    return (
-      <div>
-        <img src={message} alt='imagem de um dog' />
-        <button type='button' onClick={ this.chamaDog }>Mais um dog</button>
-      </div>
-      
-    )
-  }
-  
-  render() {
-    const {status} = this.state
-    return (
-      <div className="App">
-        {!status ? <span>Loading....</span> : this.renderDogImg()}
-      </div>
-    );
-  }
+state = {
+  message: '',
+  status: undefined,
 }
-
+// fetchDog = async () => {
+// const dadosRecebidos = await fetch('https://dog.ceo/api/breeds/image/random');
+// const objetoRecebido = await dadosRecebidos.json();
+// this.setState(
+// {
+// message: objetoRecebido.message,
+// status: objetoRecebido.status,
+// }
+// )
+// }
+componentDidMount() {
+  this.fetchDog();
+}
+shouldComponentUpdate(_a, b) {
+  if (b.message.includes('terrier')) {
+    return false;
+  }
+  return true;
+}
+componentDidUpdate() {
+  const { message } = this.state;
+  localStorage.setItem('auau', message);
+  alert(message.split('/')[4]);
+}
+fetchDog = () => {
+  fetch('https://dog.ceo/api/breeds/image/random')
+    .then((dados) => dados.json())
+    .then((obj) => this.setState({
+      message: obj.message,
+      status: obj.status,
+    }));
+}
+render() {
+  const { message, status } = this.state;
+  return (
+    !status ? <p>Loading...</p>
+      : <div>
+        <img src={ message } alt="Dog aleatório" width="300px" />
+        <button type="button" onClick={ this.fetchDog }>Próximo cachorro</button>
+      </div>
+  );
+}
+}
 export default App;
