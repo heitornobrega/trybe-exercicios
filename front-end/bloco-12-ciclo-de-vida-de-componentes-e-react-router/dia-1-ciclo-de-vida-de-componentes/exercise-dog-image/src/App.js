e6d008a38d3359195484592651d204b47fb0ed2b
heitornobrega <nobheitor@gmail.com>
2022-05-17 05:08:57 -0300

    fetch e loading implementados

--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercise-dog-image/src/App.js
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/exercise-dog-image/src/App.js
@@ -1,29 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+    this.fetchDogs = this.fetchDogs.bind(this);
+    this.renderDogImg = this.renderDogImg.bind(this);
+    this.chamaDog = this.chamaDog.bind(this);
+    
+    this.state = {
+      message: '',
+      status: undefined,
+    }
+
+  }
+  async fetchDogs() {
+    const requestReturn = await fetch('https://dog.ceo/api/breeds/image/random');
+    const requestobject = await requestReturn.json();
+    this.setState({
+      message: requestobject.message,
+      status: requestobject.status,
+    })
+  }
+
+  chamaDog() {
+    console.log('hhe dog')
+  }
+  componentDidMount(){
+    this.fetchDogs()
+  }
+
+  renderDogImg() {
+    const {message} = this.state
+    return (
+      <div>
+        <img src={message} alt='imagem de um dog' />
+        <button type='button' onClick={ this.chamaDog }>Mais um dog</button>
+      </div>
+      
+    )
+  }
+  
   render() {
+    const {status} = this.state
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={ logo } className="App-logo" alt="logo" />
-          <p>
-            Edit
-            <code>
-              src/App.js
-            </code>
-            and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {!status ? <span>Loading....</span> : this.renderDogImg()}
       </div>
     );
   }
