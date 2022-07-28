5b4cd692ab28cfb2bd61db3fecbf393de0acbb1d
heitornobrega <nobheitor@gmail.com>
2022-07-28 18:51:49 -0300

    update repo

---
+++ front-end/bloco-17-context-api-e-react-hooks/dia-1-context-api-do-react/exercise-contextAPI-refactoring/exercise-contextAPI-refactoring-exercise-three/src/context/Provider.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class Provider extends Component {
+    fetchPostsIfNeeded = () => null
+    selectSubreddit = () => null
+    refreshSubreddit = () => null
+    render() {
+        
+        return (
+            null
+        );
+    }
+}
+
+export default Provider;
