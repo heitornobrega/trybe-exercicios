7b9db3727f0ca1c64b8a52b8073af6c4ada6ea22
heitornobrega <nobheitor@gmail.com>
2022-08-28 21:27:57 -0300

    validacao do campo description

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-middlewares/ecoturismo/src/routes/activitiesController.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-middlewares/ecoturismo/src/routes/activitiesController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const activityNameValidator = require('../middleware/activityNameValidator');
 const activityPriceValidator = require('../middleware/activityPriceValidator');
+const activityDescriptionValidator = require('../middleware/activityDescriptionValidator');
 
 const activitiesRoutes = express.Router();
 
@@ -9,6 +10,7 @@ activitiesRoutes
         '/',
         activityNameValidator,
         activityPriceValidator,
+        activityDescriptionValidator,
         (req, res) => {
     res.status(201).json({ message: `A atividade ${req.body.name} foi cadastrada com sucesso!` });
 },
