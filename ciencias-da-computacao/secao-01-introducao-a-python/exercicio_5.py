cd80001e5514f4350d874066feb2ac9935381d67
heitornobrega <nobheitor@gmail.com>
2023-01-21 11:32:18 -0300

    ex 6

--- ciencias-da-computacao/secao-01-introducao-a-python/exercicio_5.py
+++ ciencias-da-computacao/secao-01-introducao-a-python/exercicio_5.py
@@ -3,7 +3,10 @@ def paint_budget(area_to_paint: float):
     amount_in_a_can_of_paint = 18
     amount_necessary = area_to_paint / ink_efficiency
     number_of_cans = amount_necessary / amount_in_a_can_of_paint
-    return number_of_cans
+    can_price = 80
+    total_price = can_price * number_of_cans
+    result = (number_of_cans, total_price)
+    return result
 
 
 print(paint_budget(120))
